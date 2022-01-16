399bbf463051dd530c74d305f85492cb49ae3915
my497 <78876345+my497@users.noreply.github.com>
2022-01-15 21:56:22 -0400

    Add files via upload

--- PricingModel.R
+++ PricingModel.R
@@ -3,6 +3,10 @@ rm(list = ls())
 # Linear regression
 library(data.table)
 library(stats)
+library(olsrr)
+library(tidyverse)
+library(ggpubr)
+library(rstatix)
 datac <-as.data.table( read.csv('dataFinal.csv'))
 datac[datac=='.'] <- NA
 datac[datac==''] <- NA
@@ -16,6 +20,8 @@ datac <- datac[Color!='10',]
 datac <- datac[Color!='11',]
 datac <- datac[Color!='12',]
 datac <- datac[month!='2',]
+
+
 #datac = datac[ Sex<5,]
 #datac = datasetc[(rowSums(is.na(datasetc))==0) ,]
 #datac = datac[complete.cases(datac),]
@@ -50,15 +56,24 @@ datac$cv_c <- as.numeric(datac$cv_c)
 datac = datac[idv_wgt<900,]
 datac = datac[idv_wgt>300,]
 
+
 datac[datac=='2.5'] <- '2'
 datac[datac=='1.5'] <-'1'
 datac <- datac[Thriftiness!='3',]
+
+# cut off all prices those are less than 90
+#datac = datac[Price>=90,]
+
+#outliers
+boxplot(datac$Price)
+datac = datac[!datac$Price %in% boxplot.stats(datac$Price)$out]
+
 
 # Regression on different pricing models
 lm1 <- lm(Price~ NoHead + I(NoHead^2) + idv_wgt+ I(idv_wgt^2) +lcf+cf+margin+cv_c+cv_lc +PreCond+Horns+Sex+Color+Frame+Muscle+Thriftiness+month+year,datac)
 print(summary(lm1))
 write.csv(summary(lm1)$coefficients, 'lm1.csv')
-#iws <- datac$idv_wgt^2
+
 lm2 <- lm(Price~ NoHead + I(NoHead^2) + idv_wgt+ I(idv_wgt^2) +lcf +cf+margin+
           cv_c+cv_lc +PreCond+PreCond:idv_wgt+Horns+Horns:idv_wgt+Sex+Sex:idv_wgt+Color+Frame
           +Frame:idv_wgt+Muscle+Muscle:idv_wgt+Thriftiness+Thriftiness:idv_wgt+month+year,datac)
@@ -69,7 +84,16 @@ Jing <- lm(Price~ NoHead + I(NoHead^2) + idv_wgt+ I(idv_wgt^2) +lcf+ lcf:idv_wgt
 ds <- lm(Price~ lcf + cf + idv_wgt + I(idv_wgt^2) + Sex:I(idv_wgt^2) + Sex:idv_wgt + lcf:idv_wgt + lcf:I(idv_wgt^2) +  cf:idv_wgt + cf:I(idv_wgt^2) + margin:idv_wgt 
                 + margin:I(idv_wgt^2) + cv_c:idv_wgt + cv_lc:idv_wgt + NoHead + I(NoHead^2) + Color + month:I(idv_wgt^2) + month:idv_wgt, datac)
            
+lm_cf_lcf <- lm(Price~ NoHead + I(NoHead^2) + idv_wgt+ I(idv_wgt^2) +lcf + lcf:idv_wgt +cf + cf:idv_wgt+margin+
+            cv_c+cv_lc +PreCond+PreCond:idv_wgt+Horns+Horns:idv_wgt+Sex+Sex:idv_wgt+Color+Frame
+          +Frame:idv_wgt+Muscle+Muscle:idv_wgt+Thriftiness+Thriftiness:idv_wgt+month+year,datac)
+lm_nomonth <- lm(Price~ NoHead + I(NoHead^2) + idv_wgt+ I(idv_wgt^2) +lcf + lcf:idv_wgt +cf + cf:idv_wgt+margin+
+                  cv_c+cv_lc +PreCond+PreCond:idv_wgt+Horns+Horns:idv_wgt+Sex+Sex:idv_wgt+Color+Frame
+                +Frame:idv_wgt+Muscle+Muscle:idv_wgt+Thriftiness+Thriftiness:idv_wgt + year,datac)
 
+
+print(summary(lm_cf_lcf))
+write.csv(summary(lm_cf_lcf)$coefficients, 'lm_cf_lcf.csv')
 
 print(summary(lm2))
 write.csv(summary(lm2)$coefficients, 'lm2.csv')
@@ -110,10 +134,11 @@ save(lm2, file = 'lm2.RData')
 save(lm1, file = 'lm1.RData')
 save(Jing, file = 'Jing.RData')
 save(ds, file = 'ds.RData')
+save(lm_cf_lcf, file = 'lm_cf_lcf.RData')
 
 write.csv(datac,'datac.csv')
 
-
+#ols_plot_dfbetas(lm_cf_lcf)
 
 
 
